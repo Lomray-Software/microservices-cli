adff8cb2fe4134e140a10ec9128566a03b5013b7
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2024-04-17 19:04:02 +0200

    feat(command): revert new program instance creation

--- src/command.js
+++ src/command.js
@@ -1,7 +1,9 @@
-import { program, Option } from 'commander';
+import { Command, Option } from 'commander';
 import fs from 'node:fs';
 import chalk from 'chalk';
 import packageJson from '../package.json' assert { type: 'json' };
+
+const program = new Command();
 
 program
   .name(packageJson.name)
